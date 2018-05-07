571f4d3d37ca895f90d1eb108fcacbfc1ac42c68
Hans-Peter Gygax <gygax@practicomp.ch>
2018-05-07 13:05:04 +0000

    Further work on header conversion script

--- src/window/dispmanx/fromc.sh
+++ src/window/dispmanx/fromc.sh
@@ -2,11 +2,24 @@
 
 h2pas -d /opt/vc/include/bcm_host.h -o bcm_host.pas
 
-cp /opt/vc/include/interface/vmcs_host/vc_dispmanx.h pp_temp/vc_dispmanx.h_1
-sed -r 's/VCHPRE_//'  pp_temp/vc_dispmanx.h_1 >pp_temp/vc_dispmanx.h_2
-sed -r 's/VCHPOST_//' pp_temp/vc_dispmanx.h_2 > pp_temp/vc_dispmanx.h_3
-cp pp_temp/vc_dispmanx.h_3 pp_temp/vc_dispmanx.h
-h2pas -i pp_temp/vc_dispmanx.h -o interface/vmcs_host/vc_dispmanx.inc
+# We need a temporary directory for "pre-processing"
+mkdir -p pp_temp
+rm -f pp_temp/*
+
+convert_header () {
+
+    #cp /opt/vc/include/interface/vmcs_host/vc_dispmanx.h pp_temp/vc_dispmanx.h_1
+    #sed -r 's/VCHPRE_//; s/VCHPOST_//' pp_temp/vc_dispmanx.h_1 >pp_temp/vc_dispmanx.h_2
+    # cp pp_temp/vc_dispmanx.h_2 pp_temp/vc_dispmanx.h
+
+    mkdir -p pp_temp/$(dirname "$1")
+
+    sed -r 's/VCHPRE_//; s/VCHPOST_//' /opt/vc/include/$1 >pp_temp/$1
+
+    h2pas -i pp_temp/$1 -o $(echo "$1" | cut -f 1 -d '.').$2 #interface/vmcs_host/vc_dispmanx.inc
+}
+
+convert_header interface/vmcs_host/vc_dispmanx.h inc
 
 # h2pas -i pp_temp/vc_dispmanx.h   -o interface/vmcs_host/vc_dispmanx.inc
 # h2paspp -dVCHPRE_ -dVCHPOST_ /opt/vc/include/interface/vmcs_host/vc_dispmanx.h -opp_temp/vc_dispmanx.h
