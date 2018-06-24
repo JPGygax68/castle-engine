ea6b89dcfcf87173536265f733de0b827e693515
Jean-Pierre Gygax <gygax@practicomp.ch>
2018-06-24 13:03:38 +0200

    DispmanX backend intro moved to wiki; removed experiments in "rpi" directory

--- rpi/rasperf3d/font.h
+++
@@ -1,9 +0,0 @@
-#ifndef FONT_H
-#define FONT_H
-
-void InitFont(uint32_t screenWidth, uint32_t screenHeight);
-void DeInitFont();
-void AddText(const char* text, int x, int y);
-void RenderText();
-
-#endif
--- rpi/rasperf3d/util/bitmap.h
+++
@@ -1,8 +0,0 @@
-#ifndef BITMAP_H
-#define BITMAP_H
-
-#include <GLES2/gl2.h>
-
-GLuint LoadBMP(const char* filename);
-
-#endif
